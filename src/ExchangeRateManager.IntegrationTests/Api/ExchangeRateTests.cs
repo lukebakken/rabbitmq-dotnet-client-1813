cebc5e26df0d52933b3fb69d16be44c035fd379c
Miguel Rodrigues <miguel.c.rodrigues@gmail.com>
2024-08-12 23:01:37 +0100

    Code cleanup;

--- src/ExchangeRateManager.IntegrationTests/Api/ExchangeRateTests.cs
+++ src/ExchangeRateManager.IntegrationTests/Api/ExchangeRateTests.cs
@@ -2,7 +2,6 @@ using DalSoft.RestClient;
 using DalSoft.RestClient.Testing;
 using ExchangeRateManager.Common.Exceptions;
 using ExchangeRateManager.Dtos;
-using ExchangeRateManager.Services;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -17,11 +16,13 @@ public class ExchangeRateTests(TestsWebApplicationFactory factory) : Integration
     [Fact]
     public async Task GetLatestExchangeRate_Returns200()
     {
+        // Act
         var response = _factory
             .CreateRestClient()
             .Resource("/ExchangeRate?fromCurrency=USD&toCurrency=EUR")
             .Get();
 
+        // Assert
         async Task<dynamic> action() => await response
             .Act<HttpResponseMessage>(x => x.StatusCode.Should().Be(HttpStatusCode.OK))
             .Act<ExchangeRateResponseDto>(x =>
@@ -36,11 +37,14 @@ public class ExchangeRateTests(TestsWebApplicationFactory factory) : Integration
     [Fact]
     public async Task GetLatestExchangeRate_InvalidCurrencies_Returns400()
     {
+        // Act
         var response = _factory
             .CreateRestClient()
             .Resource("/ExchangeRate?fromCurrency=CRASH&toCurrency=DUMMY")
             .Get();
 
+        
+        // Assert
         async Task<dynamic> action() => await response
             .Act<HttpResponseMessage>(x => x.StatusCode.Should().Be(HttpStatusCode.BadRequest))
             .Act<ProblemDetails>(x => x.Title.Should().Be(new InvalidExchangeRateException(default!).Details.Title));
