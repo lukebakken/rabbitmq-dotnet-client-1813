0ae68ba12656a1e425862d7b1a96ab32bd593004
Miguel Rodrigues <miguel.c.rodrigues@gmail.com>
2024-08-12 12:26:04 +0100

    Add Test failure logging to the integration tests. Add IntegrationTestsBase.

--- src/ExchangeRateManager.IntegrationTests/Api/ExchangeRateTests.cs
+++ src/ExchangeRateManager.IntegrationTests/Api/ExchangeRateTests.cs
@@ -1,7 +1,9 @@
 using DalSoft.RestClient;
 using DalSoft.RestClient.Testing;
 using ExchangeRateManager.Dtos;
+using ExchangeRateManager.Services;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 namespace ExchangeRateManager.Tests.IntegrationTests.Api;
@@ -9,32 +11,39 @@ namespace ExchangeRateManager.Tests.IntegrationTests.Api;
 /// <summary>
 /// Functional tests for the "ExchangeRate" endpoints
 /// </summary>
-public class ExchangeRateTests(TestsWebApplicationFactory factory) : IClassFixture<TestsWebApplicationFactory>
+public class ExchangeRateTests(TestsWebApplicationFactory factory) : IntegrationTextBase(factory), IClassFixture<TestsWebApplicationFactory>
 {
-    private readonly TestsWebApplicationFactory _factory = factory;
-
     [Fact]
     public async Task GetLatestExchangeRate_Returns200()
     {
-        await _factory
+        var response = _factory
             .CreateRestClient()
             .Resource("/ExchangeRate?fromCurrency=USD&toCurrency=EUR")
-            .Get()
+            .Get();
+
+        TestCall action = async () => await response
             .Act<HttpResponseMessage>(x => x.StatusCode.Should().Be(HttpStatusCode.OK))
             .Act<ExchangeRateResponseDto>(x =>
             {
                 x.FromCurrencyCode.Should().BeEquivalentTo("USD");
                 x.ToCurrencyCode.Should().BeEquivalentTo("EUR");
             });
+
+        await TestHandler(action);
     }
 
     [Fact]
     public async Task GetLatestExchangeRate_InvalidCurrencies_Returns400()
     {
-        await _factory
+        var response = _factory
             .CreateRestClient()
             .Resource("/ExchangeRate?fromCurrency=CRASH&toCurrency=DUMMY")
-            .Get()
-            .Act<HttpResponseMessage>(x => x.StatusCode.Should().Be(HttpStatusCode.BadRequest));
+            .Get();
+
+        TestCall action = async () => await response
+            .Act<HttpResponseMessage>(x => x.StatusCode.Should().Be(HttpStatusCode.BadRequest))
+            .Act<ProblemDetails>(x => x.Title.Should().Be(new InvalidExchangeRateException(default!).Details.Title));
+
+        await TestHandler(action);
     }
 }
---
+++ src/ExchangeRateManager.IntegrationTests/IntegrationTextBase.cs
@@ -0,0 +1,31 @@
+﻿using DalSoft.RestClient;
+
+namespace ExchangeRateManager.Tests.IntegrationTests;
+
+/// <summary>
+/// Integration tests base components.
+/// </summary>
+public class IntegrationTextBase(TestsWebApplicationFactory factory)
+{
+    protected readonly TestsWebApplicationFactory _factory = factory;
+    protected delegate Task<dynamic> TestCall();
+
+    /// <summary>
+    /// Catches any Asserts or exceptions to log the unexpected response contents before crashing.
+    /// </summary>
+    /// <param name="testCall">A delegate pointing to the test.</param>
+    protected async Task TestHandler(TestCall testCall)
+    {
+		Task<dynamic> test = Task.FromResult<dynamic>(0);
+		try
+		{
+			test = testCall();
+			await test;
+        }
+		catch
+		{
+			await test.Act<string>(x => Console.WriteLine(x));
+			throw;
+		}
+    }
+}
